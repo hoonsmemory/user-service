3b23de4f7a09f502328886770998fde41085fb25
leesh1993 <dltjdgnsok1@naver.com>
2022-09-27 19:21:46 +0900

    LoadBalanced 적용

--- src/main/java/me/hoon/userservice/config/AppConfig.java
+++ src/main/java/me/hoon/userservice/config/AppConfig.java
@@ -2,6 +2,7 @@ package me.hoon.userservice.config;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +18,7 @@ public class AppConfig {
     }
 
     @Bean
+    @LoadBalanced // ip --> Microservice name
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
