0237759034abadc8db76f76db5e44a8735913429
leesh1993 <dltjdgnsok1@naver.com>
2022-10-11 21:31:46 +0900

    resilience4j 설정

--- src/main/java/me/hoon/userservice/config/AppConfig.java
+++ src/main/java/me/hoon/userservice/config/AppConfig.java
@@ -1,13 +1,19 @@
 package me.hoon.userservice.config;
 
 import feign.Logger;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import me.hoon.userservice.exception.errordecoder.FeignErrorDecoder;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class AppConfig {
@@ -33,5 +39,27 @@ public class AppConfig {
     @Bean
     public FeignErrorDecoder feignErrorDecoder() {
         return new FeignErrorDecoder();
+    }
+
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> resilience4JCircuitBreakerFactoryCustom() {
+
+        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+                .failureRateThreshold(4)
+                .waitDurationInOpenState(Duration.ofMillis(10000))
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                .slidingWindowSize(2)
+                .build();
+
+        TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom()
+                .timeoutDuration(Duration.ofSeconds(4))
+                .build();
+
+
+        return factory -> factory.configure(builder ->
+                builder.timeLimiterConfig(timeLimiterConfig)
+                .circuitBreakerConfig(circuitBreakerConfig)
+                .build(), "orderByUserId");
     }
 }
