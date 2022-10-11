ed82dd9fdbd70fa70f5f1a9bff2f9c274a8a3277
leesh1993 <dltjdgnsok1@naver.com>
2022-10-11 21:32:11 +0900

    CircuitBreaker 적용

--- src/main/java/me/hoon/userservice/service/impl/UserServiceImpl.java
+++ src/main/java/me/hoon/userservice/service/impl/UserServiceImpl.java
@@ -1,31 +1,31 @@
 package me.hoon.userservice.service.impl;
 
 import lombok.RequiredArgsConstructor;
-
 import lombok.extern.slf4j.Slf4j;
+import me.hoon.userservice.domain.User;
 import me.hoon.userservice.domain.dto.OrderResponseDto;
 import me.hoon.userservice.domain.dto.UserDto;
 import me.hoon.userservice.domain.dto.UserRequestDto;
 import me.hoon.userservice.domain.dto.UserResponseDto;
 import me.hoon.userservice.repository.UserRepository;
 import me.hoon.userservice.service.UserService;
-import me.hoon.userservice.domain.User;
 import me.hoon.userservice.service.client.OrderServiceClient;
 import org.modelmapper.ModelMapper;
-import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Transactional
 @Slf4j
 @RequiredArgsConstructor
 @Service
@@ -37,11 +37,12 @@ public class UserServiceImpl implements UserService {
     private final Environment env;
     private final RestTemplate restTemplate;
     private final OrderServiceClient orderServiceClient;
+    private final CircuitBreakerFactory circuitBreakerFactory;
 
     @Override
     public UserResponseDto createUser(UserRequestDto userRequestDto) {
         User user = modelMapper.map(userRequestDto, User.class);
-        user.setUserId(UUID.randomUUID().toString());
+        user.setUserId(UUID.randomUUID().toString().substring(0, 10));
         user.setEncryptedPwd(passwordEncoder.encode(userRequestDto.getPwd()));
 
         userRepository.save(user);
@@ -60,7 +61,6 @@ public class UserServiceImpl implements UserService {
         String orderUrl = String.format(env.getProperty("order_service.url"), userId);
 
         //url, httpMethod, params, return
-
         /*ResponseEntity<List<OrderResponseDto>> orderListResponse = restTemplate.exchange(orderUrl, HttpMethod.GET, null,
                 new ParameterizedTypeReference<List<OrderResponseDto>>() {
 
@@ -68,7 +68,12 @@ public class UserServiceImpl implements UserService {
         );
 
         List<OrderResponseDto> orderList = orderListResponse.getBody();*/
-        List<OrderResponseDto> orderList = orderServiceClient.getOrderByUserId(userId);
+
+        //circuitBreaker 적용
+        CircuitBreaker circuitBreaker = circuitBreakerFactory.create("orderByUserId");
+        List<OrderResponseDto> orderList = circuitBreaker.run(() -> orderServiceClient.getOrderByUserId(userId),
+                throwable -> new ArrayList<>());
+
         userResponseDto.setOrders(orderList);
 
         return userResponseDto;
